570fcd4fc1f5122b7930f6b234a7d87bf3690c53
Paul <paul.hansel@colorado.edu>
2020-05-16 06:04:35 -0600

    need to fix a thing

--- src/mechanical.rs
+++ src/mechanical.rs
@@ -109,10 +109,12 @@ impl Battery {
     pub fn get_min_volume_packed(&self) -> f32 {
         let l = self.module_array.len();
         let mut vol: f32 = 0.0;
-        for i in 0..l - 1 {
-            println!("self volume: {}", self.module_array[i].module.get_min_volume_packed());
+//        println!("~~~~this is the value of L: {}", l);
+        for i in 0..l {
+ //           println!("self volume: {}", self.module_array[i].module.get_min_volume_packed());
             let vp = self.module_array[i].module.get_min_volume_packed();
-            println!("self count: {}", self.module_array[i].get_cell_count());
+ //           println!("self count: {}", self.module_array[i].get_cell_count());
+ //           // this is the wrong function to use v should be getting module coun
             let cc = self.module_array[i].get_cell_count() as f32;
             vol += vp * cc;
         }
